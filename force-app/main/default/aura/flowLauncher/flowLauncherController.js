ac4d049d2860112eb23b2e14744805f8ef3f009e
James Loghry <jamesl@demandchain.com>
2019-09-24 11:09:48 -0500

    Initial commit

---
+++ force-app/main/default/aura/flowLauncher/flowLauncherController.js
@@ -0,0 +1,20 @@
+({
+  doInit: function(component, event, helper) {
+    debugger;
+    var pageRef = component.get("v.pageReference");
+    var flow = component.find("flow");
+    var flowVariables = [];
+    Object.keys(pageRef.state).forEach(key => {
+      //Remove the c__ prefix
+      var flowVariableName = key.substr(3);
+      if (flowVariableName !== "flow") {
+        flowVariables.push({
+          name: flowVariableName,
+          type: "String",
+          value: pageRef.state[key]
+        });
+      }
+    });
+    flow.startFlow(pageRef.state.c__flow, flowVariables);
+  }
+});
