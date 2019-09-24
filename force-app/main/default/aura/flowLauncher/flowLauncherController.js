7cc44622c406edf8f48fbc8913a84ce5909c60f7
James Loghry <james.loghry@edlconsulting.com>
2019-09-24 11:48:03 -0500

    Update flowLauncherController.js

--- force-app/main/default/aura/flowLauncher/flowLauncherController.js
+++ force-app/main/default/aura/flowLauncher/flowLauncherController.js
@@ -4,14 +4,16 @@
     var flow = component.find("flow");
     var flowVariables = [];
     Object.keys(pageRef.state).forEach(key => {
-      //Remove the c__ prefix
-      var flowVariableName = key.substr(3);
-      if (flowVariableName !== "flow") {
-        flowVariables.push({
-          name: flowVariableName,
-          type: "String",
-          value: pageRef.state[key]
-        });
+      if (key.startsWith("c__")) {
+        //Remove the c__ prefix
+        var flowVariableName = key.substr(3);
+        if (flowVariableName !== "flow") {
+          flowVariables.push({
+            name: flowVariableName,
+            type: "String",
+            value: pageRef.state[key]
+          });
+        }
       }
     });
     flow.startFlow(pageRef.state.c__flow, flowVariables);
